9d95a118c52207651a881edc438fba680bc4ac45
NguyenLanAnh2299 <43206459+NguyenLanAnh2299@users.noreply.github.com>
2019-12-18 18:47:36 +0700

    feature create models

--- AssignmentEAP/Models/DisciplineStudent.cs
+++ AssignmentEAP/Models/DisciplineStudent.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +8,13 @@ namespace AssignmentEAP.Models
 {
     public class DisciplineStudent
     {
+        [Key]
+        public int DisciplineStudent_id { get; set; }
+        public double Discipline_Value { get; set; }
+        public DateTime Created_at { get; set; }
+        public DateTime Updated_at { get; set; }
+        public DateTime Deleted_at { get; set; }
+        public virtual Student Student { get; set; }
+        public virtual Discipline Discipline { get; set; }
     }
 }
--- AssignmentEAP/Models/Student.cs
+++ AssignmentEAP/Models/Student.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +9,26 @@ namespace AssignmentEAP.Models
 {
     public class Student
     {
+        [Key]
+        public string RollNumber { get; set; }
+
+        [DisplayName(" Student Name")]
+        public string Student_Name { get; set; }
+        public string Avatar { get; set; }
+
+        [DisplayName("Birth Day")]
+        [DataType(DataType.DateTime)]
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:yyyy-MM-dd}")]
+        public DateTime Birthday { get; set; }
+
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        [DisplayName("Class")]
+        public int Class_Id { get; set; }
+        public virtual Class Class { get; set; }
+        public DateTime Created_at { get; set; }
+        public DateTime Updated_at { get; set; }
+        public DateTime Deleted_at { get; set; }
     }
 }
