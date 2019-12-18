ac149fe6981daab3201b6d69e3df517f9f7bfca6
NguyenLanAnh2299 <43206459+NguyenLanAnh2299@users.noreply.github.com>
2019-12-18 18:00:13 +0700

    feature create models

---
+++ AssignmentEAP/Models/Class.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace AssignmentEAP.Models
+{
+    public class Class
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Class_Id { get; set; }
+        public string Class_name { get; set; }
+        public DateTime Created_at { get; set; }
+        public DateTime Updated_at { get; set; }
+        public DateTime Deleted_at { get; set; }
+        public virtual ICollection<Student> Students { get; set; }
+    }
+}
