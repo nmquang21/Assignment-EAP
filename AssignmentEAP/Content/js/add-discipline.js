0f2126fe0bc1d1f7cbed5c68a1359336874efc2c
Quang <nmquang21@gmail.com>
2019-12-20 01:02:39 +0700

    xu li them loi sinh vien

---
+++ AssignmentEAP/Content/js/add-discipline.js
@@ -0,0 +1,17 @@
+﻿jQuery(document).ready(function () {
+    jQuery('.add-discipline').click(function () {
+        jQuery.ajax({
+            method : 'GET',
+            url: '/Discipline',
+            async: false
+        }).done(function (data) {
+            var html = '';
+            jQuery.each(data, function (index, value) {
+                var x = `<option value=${value.id}>${value.name}</option>`;
+                jQuery('.select-discipline').append(x);
+            })
+         
+            jQuery('#Modal-Add-Discipline').modal();
+        })
+    })
+})
---
+++ AssignmentEAP/Controllers/DisciplineController.cs
@@ -0,0 +1,20 @@
+﻿using AssignmentEAP.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AssignmentEAP.Controllers
+{
+    public class DisciplineController : Controller
+    {
+        private MyDbContext db = new MyDbContext();
+        // GET: Discipline
+        public ActionResult Index()
+        {
+            var listDisciplines = db.Disciplines.Select(c => new { id = c.Discipline_id, name = c.Discipline_name}).ToList();
+            return Json(listDisciplines, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
