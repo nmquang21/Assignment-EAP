71ea3204fca789f6934fb8c23a8d451a41054958
nguyen128 <tvngiagia@gmail.com>
2019-12-22 14:13:40 +0700

    Add chart thong ke cho moi lop hoc.

--- AssignmentEAP/Models/MyDbContext.cs
+++ AssignmentEAP/Models/MyDbContext.cs
@@ -19,6 +19,8 @@ namespace AssignmentEAP.Models
 
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
+            Database.SetInitializer<MyDbContext>(null);
+            base.OnModelCreating(modelBuilder);
         }
     }
 }
